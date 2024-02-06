2e51d6b6f9d22c8b3c918ea4213ea81424fcb1bf
Omerrizabahadir <omrbahadir@gmail.com>
2024-02-06 19:02:41 +0300

    Taking numbers from users and adding these numbers to the array

---
+++ Homework/src/homework04/Main.java
@@ -0,0 +1,54 @@
+package homework04;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Taking numbers from users and adding numbers to the array
+        kullanıcıdan kaç adet sayı gireceğini al ve sayı girdirt ve sonra bunları diziye ata
+        sonra bu sayıların ortalamasını,toplamını, tek çift değerlerini yazan program
+         */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Dizinizin kaç elemanlı olmasını istersiniz?");
+        int userNumber = scanner.nextInt();
+        int[] userNumberArray = new int[userNumber];
+        int sum=0;
+        int average=0;
+        String tekSayi=" Tek sayı";
+        String ciftSayi=" Çift Sayı";
+
+
+
+        for (int i = 0; i < userNumber; i++) {
+            System.out.println(i+". elemanı giriniz");
+            userNumberArray[i] = scanner.nextInt();
+            sum=sum+userNumberArray[i];
+            if (userNumberArray[i]%2==0){
+
+                System.out.println(userNumberArray[i]+ciftSayi);
+            }else{
+
+
+                System.out.println(userNumberArray[i]+tekSayi);
+            }
+        }
+
+        for (int i = 0; i < userNumberArray.length; i++) {
+        }
+
+
+        System.out.println("Oluşturduğunuz dizi :" + Arrays.toString(userNumberArray));
+        System.out.println("Dizideki sayıların toplamı :"+sum);
+        System.out.println("Dizideki sayıların toplamı :"+(double)sum/userNumber);
+
+
+    }
+
+}
+
+
+
