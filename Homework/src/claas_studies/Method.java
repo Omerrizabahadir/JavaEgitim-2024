3719a56bd550dbcdcc99d2caef85e114f7fe7f50
Omerrizabahadir <omrbahadir@gmail.com>
2024-02-27 18:33:53 +0300

    Homework 03 Birds type count

---
+++ Homework/src/claas_studies/Method.java
@@ -0,0 +1,35 @@
+package claas_studies;
+
+public class Method {
+
+
+
+
+    public static void main(String[] args) {
+        int [] birdTypes=new int[6];
+        int [] givenBirdTypesCount={5,5,5,1,1,1,3,3,3};
+        int max=0;
+
+
+
+
+
+        for(int birdType:givenBirdTypesCount){
+            birdTypes[birdType]++;
+            if(birdTypes[birdType]>max){
+                max=birdTypes[birdType];
+            }
+          for (int i=0;i< birdTypes.length;i++){
+              if (birdTypes[i]==max){
+                  System.out.println("en çok tekrar eden tür: "+i);
+                  break;
+              }
+
+          }
+
+        }
+
+
+    }
+
+}
