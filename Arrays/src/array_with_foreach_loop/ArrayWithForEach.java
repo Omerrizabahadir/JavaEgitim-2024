1cab7f9b5609045f2f2d77e410909fcf06ef1642
Omerrizabahadir <omrbahadir@gmail.com>
2024-02-27 18:35:07 +0300

    Array (for each loop)

---
+++ Arrays/src/array_with_foreach_loop/ArrayWithForEach.java
@@ -0,0 +1,11 @@
+package array_with_foreach_loop;
+
+public class ArrayWithForEach {
+    public static void main(String[] args) {
+        int [] age={12,4,5};
+
+        for(int a:age){
+            System.out.println(a);
+        }
+    }
+}
