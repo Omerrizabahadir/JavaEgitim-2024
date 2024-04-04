ff12c2c7e4c87ee2e264801e3f65de5042fe6921
Omerrizabahadir <omrbahadir@gmail.com>
2024-04-04 14:22:03 +0300

    Java 8  homework03(Optional  class using with orElse (), orElseGet() methods)

---
+++ Java8/src/optional_homework_03/Main.java
@@ -0,0 +1,25 @@
+package optional_homework_03;
+
+import java.util.Optional;
+
+public class Main {
+    Integer number= null;
+
+
+    public static void main(String[] args) {
+
+        Main main=new Main();
+        Integer result1=Optional.ofNullable(main.number)
+                .orElse(0);
+
+        System.out.println("Number : "+main.number);
+
+        System.out.println("******************");
+
+
+        Integer result2=Optional.of(main)
+                .map(e->e.number)
+                .orElseGet(()->1);
+        System.out.println("Number : "+result2);
+    }
+}
