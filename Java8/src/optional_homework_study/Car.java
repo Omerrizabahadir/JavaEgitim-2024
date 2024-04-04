865de3dd8154fcc9c7e029c5887abd256bd0d273
Omerrizabahadir <omrbahadir@gmail.com>
2024-04-04 13:53:48 +0300

    Java 8 (Optional Class orElse(), orElseGet(), methods homework01)

---
+++ Java8/src/optional_homework_study/Car.java
@@ -0,0 +1,32 @@
+package optional_homework_study;
+
+public class Car {
+    private  String brand;
+    private  String model;
+    private String color;
+    private Integer year;
+
+
+    public Car(String brand, String model, String color, Integer year) {
+        this.brand = brand;
+        this.model = model;
+        this.color=color;
+        this.year = year;
+
+    }
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
