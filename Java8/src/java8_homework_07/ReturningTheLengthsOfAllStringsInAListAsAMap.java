52c5fc6a40698f58c0753b8b61bd515cff448f1e
Omerrizabahadir <omrbahadir@gmail.com>
2024-04-06 22:18:39 +0300

    Java 8 List (Mapping the lengths of all string words in a list using Collection Map Interface)

---
+++ Java8/src/java8_homework_07/ReturningTheLengthsOfAllStringsInAListAsAMap.java
@@ -0,0 +1,29 @@
+package java8_homework_07;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ReturningTheLengthsOfAllStringsInAListAsAMap {
+    /*
+    Bir liste içerisindeki tüm stringlerin uzunluklarını
+    bir harita (Map) olarak döndüren bir programı yazınız.
+     */
+
+    public static void main(String[] args) {
+
+
+        List<String> listOfFruits = Arrays.asList("muz", "elma", "armut", "çilek");
+
+        Map<String, Integer> mapList = listOfFruits.stream()
+                .collect(Collectors.toMap(p -> p, p -> p.length()));
+
+        System.out.println(mapList);
+
+
+    }
+}
+
